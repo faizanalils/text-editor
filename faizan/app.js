89a9e17f30b3a57f993576a5d16ef9ef893172fa
faizanalils <126968734+faizanalils@users.noreply.github.com>
2023-05-18 20:36:11 +0500

    Add files via upload

---
+++ faizan/app.js
@@ -0,0 +1,13 @@
+// var numbers = [12, 20, 50, 18.90, 39, 20, 01, 190, 75, 60];
+
+// function giveLargestNumber() {
+//   return Math.max(...numbers);
+// }
+
+// var largestNumber = giveLargestNumber();
+// console.log('largestNumber ->', largestNumber); 
+
+
+function paragraphexpands () {
+    document.getElementById("paragraph")("")
+}
